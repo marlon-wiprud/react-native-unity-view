e2cb169f698eabe14603a18064292fdda3298515
Marlon Wiprud <marlonwiprud@Marlons-MBP.home>
2019-10-18 08:35:48 -0400

    modify RCTBridge and RNEventEmitter imports

--- ios/UnityNativeModule.h
+++ ios/UnityNativeModule.h
@@ -6,8 +6,18 @@
 //  Copyright © 2018 xzper. All rights reserved.
 //
 
-#import <React/RCTEventDispatcher.h>
+#if __has_include("RCTBridgeModule.h")
+#import "RCTBridgeModule.h"
+#else
 #import <React/RCTBridgeModule.h>
+#endif
+
+#if __has_include("RNEventEmitter.h")
+#import "RNEventEmitter.h"
+#else
+#import <RNEventEmitter/RNEventEmitter.h>
+#endif
+
 #import "UnityUtils.h"
 
 @interface UnityNativeModule : NSObject <RCTBridgeModule, UnityEventListener>
